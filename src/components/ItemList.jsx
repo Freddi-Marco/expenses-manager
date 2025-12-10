d006f42eb655e69712460c11aae2c78e0fb9bc4a
Freddi-Marco <marcofreddi07@gmail.com>
2025-12-10 11:08:28 +0100

    Aggiunta ricerca in ItemList

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,16 +1,31 @@
 import React, { useState } from "react";
 import ExpenseItem from "./Item";
 
-const ExpenseList = ({ items, onDelete }) => (
-  <div className="flex flex-col gap-3 mt-0 w-full">
-    {items.length === 0 ? (
-      <div className="text-gray-400 text-center font-inter">Nessuna spesa</div>
-    ) : (
-      items.map((item, idx) => (
-        <ExpenseItem key={item.id || idx} {...item} onDelete={onDelete} />
-      ))
-    )}
-  </div>
-);
+const ExpenseList = ({ items, onDelete }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredItems = items.filter(item =>
+    item.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return(
+    <div className="flex flex-col gap-3 mt-0 w-full">
+      <input
+        type="text"
+        placeholder="Cerca..."
+        className="border border-gray-300 p-2 rounded"
+        value={searchTerm}
+        onChange={e => setSearchTerm(e.target.value)}
+      />
+      {filteredItems.length === 0 ? (
+        <div className="text-gray-400 text-center font-inter">Nessuna spesa</div>
+      ) : (
+        filteredItems.map((item, idx) => (
+          <ExpenseItem key={item.id || idx} {...item} onDelete={onDelete} />
+        ))
+      )}
+    </div>
+  )
+};
 
 export default ExpenseList;
